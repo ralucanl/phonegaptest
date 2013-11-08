2c00c2df9224da309343b5e69b64b5f6af841f57
raluca <raluca@nextlogic.ro>
2013-11-08 16:54:34 +0200

    sec commit

--- phonegaptest-master/www/js/index.js
+++ phonegaptest-master/www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+/*var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -47,3 +47,4 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+*/
